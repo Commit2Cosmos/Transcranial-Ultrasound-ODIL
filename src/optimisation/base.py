d14384b9826eb5b48a9765e07941315662ebb2e4
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 12:22:14 +0100

    feat: update optimiser params

--- src/optimisation/base.py
+++ src/optimisation/base.py
@@ -1,16 +1,14 @@
 from abc import ABC, abstractmethod
-from typing import Callable
 
-# import torch
 import scipy.optimize as scopt
+from src.loss import DiscreteLoss
 
 
 class Optimiser(ABC):
     """Base optimiser class"""
 
-    def __init__(self, loss_fn: Callable) -> None:
-        self.loss_fn = loss_fn  # loss function
-        self.grad_fn = None  # autograd(loss_fn)  # gradient provided by JAX autodiff
+    def __init__(self, loss: DiscreteLoss) -> None:
+        self.loss = loss  # loss function
 
     @abstractmethod  # to be implemented by classes that inherit
     def minimise(self, u0, **kwargs):
@@ -20,17 +18,17 @@ class Optimiser(ABC):
 class ScipyOptimiser(Optimiser):
     """Wrapper around scipy.optimize.minimize"""
 
-    def __init__(self, loss_fn: Callable, method: str = "L-BFGS-B", **opts) -> None:
-        super().__init__(loss_fn)
+    def __init__(self, loss: DiscreteLoss, method: str = "L-BFGS-B", **opts) -> None:
+        super().__init__(loss)
         self.method = method  # e.g., 'L-BFGS-B', 'Newton-CG'
         self.opts = opts  # e.g., maxiter, ftol
 
     def minimise(self, u0, callback=None) -> scopt.OptimizeResult:
         result = scopt.minimize(
-            fun=self.loss_fn,
+            fun=self.loss.evaluate,
             x0=u0,
             method=self.method,
-            jac=self.grad_fn,
+            jac=True,  # gradient provided by torch through .evaluate
             callback=callback,
             options=self.opts,
         )
@@ -41,14 +39,6 @@ class LBFGSB(ScipyOptimiser):
     """Subclass for L-BFGS method"""
 
     def __init__(
-        self, loss_fn: Callable, maxiter: int = 500, ftol: float = 1e-8, **opts
+        self, loss: DiscreteLoss, maxiter: int = 500, ftol: float = 1e-8, **opts
     ) -> None:
-        super().__init__(loss_fn, method="L-BFGS-B", maxiter=maxiter, ftol=ftol, **opts)
-
-
-if __name__ == "__main__":
-
-    def test_loss():
-        return 0
-
-    optimiser = LBFGSB(test_loss)
+        super().__init__(loss, method="L-BFGS-B", maxiter=maxiter, ftol=ftol, **opts)
