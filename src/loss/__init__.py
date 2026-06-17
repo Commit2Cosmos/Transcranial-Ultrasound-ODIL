a54118214edb064148fd23b983fac2590f179fcf
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 12:18:49 +0100

    feat: update imports

--- src/loss/__init__.py
+++ src/loss/__init__.py
@@ -1,3 +1,6 @@
 from .utils import LossConfig, LossTape
+from .base import DiscreteLoss
+from .forward import ForwardLoss
+from .inverse import InverseLoss
 
-__all__ = ["LossConfig", "LossTape"]
+__all__ = ["LossConfig", "LossTape", "DiscreteLoss", "ForwardLoss", "InverseLoss"]
