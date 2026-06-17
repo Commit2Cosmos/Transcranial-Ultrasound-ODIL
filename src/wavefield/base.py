d50caf15ae142d5375670c92da0369ec81677e32
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 15:38:58 +0100

    feat: plotting util

--- src/wavefield/base.py
+++ src/wavefield/base.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 import numpy as np
+import matplotlib.pyplot as plt
 
 from src.grid import Grid
 
@@ -64,3 +65,61 @@ class Wavefield:
 
         self._amplitude = flat[:n_amp].reshape(Nt, Nx, Ny)
         self._wavespeed = flat[n_amp:].reshape(Nx, Ny)
+
+    def show(self, title="Wavefield and model", view: str = "xy", idx=None):
+        assert view in [
+            "xy",
+            "ty",
+            "tx",
+        ], "View must be str and one of 'xy', 'ty', 'tx'"
+        axis = {"xy": 0, "ty": 1, "tx": 2}[view]  # extract axis index
+
+        # extrcat shapes
+        Nx, Ny = self.grid.shape
+        Nt = self.grid.nt
+
+        # default to centre of chosen plane
+        if idx is None:
+            param = [Nt, Nx, Ny][axis]  # extract upper limit of axis
+            idx = param // 2
+
+        assert (
+            idx >= 0 and idx < param
+        ), f"idx should be an int in range [0, {param}], got {idx}."
+
+        amp_data = np.take(self.amplitude, idx, axis=axis)  # extract slice
+
+        fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+        (xmin, xmax), (ymin, ymax) = self.grid.extent
+
+        im1 = plt.imshow(
+            amp_data,
+            ax=axs[0],
+            origin="lower",
+            extent=(xmin, xmax, ymin, ymax),
+            cmap="RdBu_r",
+            aspect="auto",
+            title=f"Amplitude field ({view} plane, idx = {idx})",
+        )
+
+        im2 = plt.imshow(
+            self.wavespeed,
+            ax=axs[1],
+            origin="lower",
+            extent=(xmin, xmax, ymin, ymax),
+            cmap="viridis",
+            aspect="auto",
+            title="Wave speed model",
+        )
+
+        i, j = view[0], view[1]  # extract letters for labelling
+        for ax in axs:
+            ax.set_xlabel(i)
+            ax.set_ylabel(j)
+
+        plt.colorbar(im1, cax=axs[0], title="Amplitude")
+        plt.colorbar(im2, cax=axs[1], title=r"Wavespeed ($ms^{-1}$)")
+
+        fig.suptitle(title)
+        fig.tight_layout()
+        fig.show()
