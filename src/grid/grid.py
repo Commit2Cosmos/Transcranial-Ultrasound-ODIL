8fc1074fbfde81aa7f091c7b32b834823b210682
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 14:15:53 +0100

    fix: remove kw unpacking

--- src/grid/grid.py
+++ src/grid/grid.py
@@ -56,6 +56,7 @@ class Grid:
     def __post_init__(self):
         self.interior_nx, self.interior_ny = self.interior_shape
         (ix_min, ix_max), (iy_min, iy_max) = self.interior_extent
+        device = torch.device(self.device)
 
         self.dx = (ix_max - ix_min) / (self.interior_nx - 1)
         self.dy = (iy_max - iy_min) / (self.interior_ny - 1)
@@ -78,10 +79,11 @@ class Grid:
             self.nt = int(math.ceil(self.t_max / (self.cfl_safety * dt_cfl))) + 1
         self.dt = self.t_max / (self.nt - 1)
 
-        kw = dict(dtype=self.dtype, device=self.device)
-        self.x = torch.linspace(x_min, x_max, self.nx, **kw)
-        self.y = torch.linspace(y_min, y_max, self.ny, **kw)
-        self.t = torch.linspace(0.0, self.t_max, self.nt, **kw)
+        self.x = torch.linspace(x_min, x_max, self.nx, dtype=self.dtype, device=device)
+        self.y = torch.linspace(y_min, y_max, self.ny, dtype=self.dtype, device=device)
+        self.t = torch.linspace(
+            0.0, self.t_max, self.nt, dtype=self.dtype, device=device
+        )
         self.X, self.Y = torch.meshgrid(self.x, self.y, indexing="ij")
 
         self.sigma_x, self.sigma_y = self._build_pml_profiles()
@@ -114,15 +116,18 @@ class Grid:
         """sigma_x(i, j), sigma_y(i, j) on the full grid; zero in the interior."""
         p = self.pml_width
         if p == 0:
-            zeros = torch.zeros(self.nx, self.ny, dtype=self.dtype, device=self.device)
+            zeros = torch.zeros(
+                self.nx, self.ny, dtype=self.dtype, device=torch.device(self.device)
+            )
             return zeros, zeros
         L_pml_x = p * self.dx
         L_pml_y = p * self.dy
         sigma_max_x = self._sigma_max(L_pml_x)
         sigma_max_y = self._sigma_max(L_pml_y)
 
-        i = torch.arange(self.nx, dtype=self.dtype, device=self.device)
-        j = torch.arange(self.ny, dtype=self.dtype, device=self.device)
+        device = torch.device(self.device)
+        i = torch.arange(self.nx, dtype=self.dtype, device=device)
+        j = torch.arange(self.ny, dtype=self.dtype, device=device)
 
         d_x = (
             torch.clamp(p - i, min=0.0) + torch.clamp(i - (self.nx - 1 - p), min=0.0)
