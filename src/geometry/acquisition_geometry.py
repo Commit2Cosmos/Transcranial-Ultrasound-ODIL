ad6085ad1baa000a7557742f8aaef1efdcd9359b
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 16:41:04 +0100

    fix: import paths

--- src/geometry/acquisition_geometry.py
+++ src/geometry/acquisition_geometry.py
@@ -6,8 +6,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
-from grid.grid import Grid
-from models.velocity_models import VelocityModel
+from src.grid import Grid
+from src.models import VelocityModel
 
 
 class AcquisitionGeometry:
---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+from .velocity_models import VelocityModel
+
+__all__ = ["VelocityModel"]
