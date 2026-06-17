92327525a498b4db8aa52fd73a35ac77f4dae279
ada-ld2022 <luke.dinsdale22@imperial.ac.uk>
2026-06-17 12:13:32 +0100

    update: use _eval_loss abstract method

--- src/loss/forward.py
+++ src/loss/forward.py
@@ -12,15 +12,19 @@ class ForwardLoss(DiscreteLoss):
     ) -> Tuple[float, torch.Tensor]:
         wavefield.requires_grad_()
         r = self._residuals(wavefield, wavespeed)
-        L = (r**2).sum()
-        self.callback.log(L.item(), r)
+        L = self._eval_loss(r)
         L.backward()
         grad = (
             wavefield.grad
             if wavefield.grad is not None
             else torch.zeros_like(wavefield)
         )
+
+        self.callback.log(L.item(), r)
         return L.item(), grad
+
+    def _eval_loss(self, residuals: torch.Tensor) -> torch.Tensor:
+        return (residuals**2).sum()  # unnormalised for forward problem
 
     def _residuals(self, wavefield, wavespeed) -> torch.Tensor:
         r_pde = self._eval_pde_loss(wavefield, wavespeed)
