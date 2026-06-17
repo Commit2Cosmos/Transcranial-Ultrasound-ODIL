ee1d00f497c98e17f9664badfc0e537d4d6b9514
ada-mp425 <milica.pejic25@ic.ac.uk>
2026-06-17 09:37:54 +0100

    stencil operators and initial conditions added for sparse

--- src/operator/__init__.py
+++ src/operator/__init__.py
@@ -1,3 +1,20 @@
 from base import SparseOperator, DenseOperator
+from .conditions import(
+    InitialConditions,
+    NeumannMirrorBC2nd,
+    NeumannMirrorBC4th,
+)
+from .spatial import Laplacian2ndOrder, Laplacian4thOrder
+from .temporal import TimeOperator2ndOrder, TimeOperator4thOrder
 
-__all__ = ["SparseOperator", "DenseOperator"]
+__all__ = [
+    "SparseOperator",
+    "DenseOperator",
+    "InitialConditions",
+    "Laplacian2ndOrder",
+    "Laplacian4thOrder",
+    "NeumannMirrorBC2nd",
+    "NeumannMirrorBC4th",
+    "TimeOperator2ndOrder",
+    "TimeOperator4thOrder",
+]
