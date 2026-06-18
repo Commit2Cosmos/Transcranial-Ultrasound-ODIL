c1688c0782be0aa003a4e771e7bc9b246a64b91b
Luke Dinsdale <lukedinsdale43@gmail.com>
2026-06-18 13:20:53 +0100

    feat: make optimiser ingest Wavefield and problem agnostic

--- src/optimisation/base.py
+++ src/optimisation/base.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
 
 import scipy.optimize as scopt
-from src.loss import DiscreteLoss
+from loss import DiscreteLoss, ForwardLoss, InverseLoss
+from wavefield import Wavefield
 
 
 class Optimiser(ABC):
     """Base optimiser class"""
 
-    def __init__(self, loss: DiscreteLoss) -> None:
+    def __init__(self, wavefield: Wavefield, loss: DiscreteLoss) -> None:
         self.loss = loss  # loss function
+        self.wavefield = wavefield  # wavefield to optimise
 
     @abstractmethod  # to be implemented by classes that inherit
     def minimise(self, u0, **kwargs):
@@ -18,12 +20,20 @@ class Optimiser(ABC):
 class ScipyOptimiser(Optimiser):
     """Wrapper around scipy.optimize.minimize"""
 
-    def __init__(self, loss: DiscreteLoss, method: str = "L-BFGS-B", **opts) -> None:
-        super().__init__(loss)
+    def __init__(
+        self, wavefield: Wavefield, loss: DiscreteLoss, method: str = "L-BFGS-B", **opts
+    ) -> None:
+        super().__init__(wavefield, loss)
         self.method = method  # e.g., 'L-BFGS-B', 'Newton-CG'
         self.opts = opts  # e.g., maxiter, ftol
 
-    def minimise(self, u0, callback=None) -> scopt.OptimizeResult:
+    def minimise(self, callback=None) -> scopt.OptimizeResult:
+        # use amplitude data only for the forward
+        if isinstance(self.loss, ForwardLoss):
+            u0 = self.wavefield.amplitude.ravel()
+        elif isinstance(self.loss, InverseLoss):
+            u0 = self.wavefield.flat_data
+
         result = scopt.minimize(
             fun=self.loss.evaluate,
             x0=u0,
@@ -39,6 +49,13 @@ class LBFGSB(ScipyOptimiser):
     """Subclass for L-BFGS method"""
 
     def __init__(
-        self, loss: DiscreteLoss, maxiter: int = 500, ftol: float = 1e-8, **opts
+        self,
+        wavefield: Wavefield,
+        loss: DiscreteLoss,
+        maxiter: int = 500,
+        ftol: float = 1e-8,
+        **opts,
     ) -> None:
-        super().__init__(loss, method="L-BFGS-B", maxiter=maxiter, ftol=ftol, **opts)
+        super().__init__(
+            wavefield, loss, method="L-BFGS-B", maxiter=maxiter, ftol=ftol, **opts
+        )
