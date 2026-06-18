9cbdf4f03351bcdc9ad6b865499ab90c13bc731c
Luke Dinsdale <lukedinsdale43@gmail.com>
2026-06-18 16:22:31 +0100

    feat: update include path

--- src/operator/__init__.py
+++ src/operator/__init__.py
@@ -6,6 +6,7 @@ from .conditions import (
 )
 from .spatial import Laplacian2ndOrder, Laplacian4thOrder
 from .temporal import TimeOperator2ndOrder, TimeOperator4thOrder
+from .utils import WaveEquation
 
 __all__ = [
     "SparseOperator",
@@ -17,4 +18,5 @@ __all__ = [
     "NeumannMirrorBC4th",
     "TimeOperator2ndOrder",
     "TimeOperator4thOrder",
+    "WaveEquation",
 ]
