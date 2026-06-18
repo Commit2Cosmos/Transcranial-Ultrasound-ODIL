0690bf052f95f43f0c2ee6cd3323fa0a69aefba7
Luke Dinsdale <lukedinsdale43@gmail.com>
2026-06-18 13:55:37 +0100

    feat: add reshaping and wavespeed extraction

--- src/loss/forward.py
+++ src/loss/forward.py
@@ -1,6 +1,5 @@
 from typing import Tuple
 from .base import DiscreteLoss
-from .utils import LossConfig, LossTape
 
 import torch
 import numpy as np
@@ -9,13 +8,18 @@ import numpy as np
 class ForwardLoss(DiscreteLoss):
     """Loss function for the forward problem."""
 
-    def __init__(self, wavespeed, config: LossConfig, callback: LossTape | None = None):
-        super().__init__(config, callback)
-        self.c: torch.Tensor = wavespeed
-
     def evaluate(self, data: np.ndarray) -> Tuple[float, np.ndarray]:
         d = torch.tensor(data, requires_grad=True, dtype=torch.float64)
-        r = self._residuals(d)
+
+        # extract and reshape amplitude
+        Nt = self.config.wavefield.grid.nt
+        Nx, Ny = self.config.wavefield.grid.shape
+        amp = d[: self.config.speed_offset].reshape(Nt, Nx, Ny)
+
+        #  get wavespeed separately, we need it to compute the PDE residuals
+        wsp = self.config.wavefield.wavespeed
+
+        r = self._residuals(amp, wsp)
         L = self._eval_loss(r)
         L.backward()
         grad = d.grad if d.grad is not None else torch.zeros_like(d)
@@ -29,11 +33,11 @@ class ForwardLoss(DiscreteLoss):
     def _eval_loss(self, residuals: torch.Tensor) -> torch.Tensor:
         return (residuals**2).mean()
 
-    def _residuals(self, data: torch.Tensor) -> torch.Tensor:
-        r_pde = self._eval_pde_loss(data)
+    def _residuals(self, amp: torch.Tensor, wsp: torch.Tensor) -> torch.Tensor:
+        r_pde = self._eval_pde_loss(amp, wsp)
         return r_pde
 
-    def _eval_pde_loss(self, data: torch.Tensor):
-        utt = self.time_op.apply(data)
-        lap = self.lap.apply(data)
-        return utt - (self.c**2) * lap  # u_tt - c^2(u_xx + u_yy)
+    def _eval_pde_loss(self, amp, wsp):
+        utt = self.time_op.apply(amp)
+        lap = self.lap.apply(amp)
+        return utt - (wsp**2) * lap  # u_tt - c^2(u_xx + u_yy)
