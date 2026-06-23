b6aa3a5025dd02fcd10231654903bff4dd9ffe57
ada-ab2125 <ab2125@ic.ac.uk>
2026-06-23 14:37:21 +0100

    feat: add regulariser

--- odil_wave/__init__.py
+++ odil_wave/__init__.py
@@ -1,13 +1,21 @@
 from odil_wave.grid import Grid
+from odil_wave.source import SourceSignal
 from odil_wave.geometry import AcquisitionGeometry
 from odil_wave.models import VelocityModel
 from odil_wave.wavefield import Wavefield
 from odil_wave.operator import WaveEquation
-from odil_wave.loss import LossConfig, LossTape, ForwardLoss, InverseLoss
+from odil_wave.loss import (
+    LossConfig,
+    LossTape,
+    ForwardLoss,
+    InverseLoss,
+    Regulariser,
+)
 from odil_wave.optimisation import LBFGSB
 
 __all__ = [
     "Grid",
+    "SourceSignal",
     "AcquisitionGeometry",
     "VelocityModel",
     "Wavefield",
@@ -16,5 +24,6 @@ __all__ = [
     "LossTape",
     "ForwardLoss",
     "InverseLoss",
+    "Regulariser",
     "LBFGSB",
 ]
