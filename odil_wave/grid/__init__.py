7e332cc2ac019b4fa7bca226dd2c06ea14c20bf5
ada-mp425 <milica.pejic25@ic.ac.uk>
2026-07-20 15:56:05 +0100

    export frequency selection

--- odil_wave/grid/__init__.py
+++ odil_wave/grid/__init__.py
@@ -1,3 +1,15 @@
 from .grid import Grid
+from .frequency_selection import (
+    FrequencySelection,
+    complex_dtype,
+    discrete_lambda_t,
+    discrete_lambda_tt,
+)
 
-__all__ = ["Grid"]
+__all__ = [
+    "Grid",
+    "FrequencySelection",
+    "complex_dtype",
+    "discrete_lambda_t",
+    "discrete_lambda_tt",
+]
