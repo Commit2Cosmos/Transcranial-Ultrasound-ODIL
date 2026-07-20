5ad0bbe0453814a5ea1a49b5e0d751fb38042593
ada-mp425 <milica.pejic25@ic.ac.uk>
2026-07-20 15:56:30 +0100

    Sponge, Helmholtz solver

--- odil_wave/operator/__init__.py
+++ odil_wave/operator/__init__.py
@@ -2,11 +2,14 @@ from .base import SparseOperator, DenseOperator
 from .conditions import (
     NeumannMirrorBC2nd,
     NeumannMirrorBC4th,
+    Sponge,
+    PML,
 )
 from .spatial import Laplacian2ndOrder, Laplacian4thOrder, Laplacian10thOrder
 from .temporal import TimeOperator2ndOrder, TimeOperator4thOrder
 from .utils import WaveEquation
 from .leapfrog import LeapfrogSolver
+from .helmholtz import HelmholtzSolver
 
 __all__ = [
     "SparseOperator",
@@ -16,8 +19,12 @@ __all__ = [
     "Laplacian10thOrder",
     "NeumannMirrorBC2nd",
     "NeumannMirrorBC4th",
+    "Sponge",
+    "PML",
     "TimeOperator2ndOrder",
     "TimeOperator4thOrder",
     "WaveEquation",
     "LeapfrogSolver",
+    "HelmholtzSolver",
 ]
+
