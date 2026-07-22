cec543b1c2becb63fa6c731e908430b4575e9e1c
ada-mp425 <milica.pejic25@ic.ac.uk>
2026-07-22 13:29:42 +0100

    export frequency continuation

--- odil_wave/optimisation/__init__.py
+++ odil_wave/optimisation/__init__.py
@@ -1,8 +1,18 @@
 from .base import LBFGSB
 from .c_step_debug import debug_one_c_step
+from .frequency_continuation import (
+    BandTimingStats,
+    FrequencyBand,
+    FrequencyContinuationResult,
+    run_frequency_continuation,
+)
 
 __all__ = [
     "LBFGSB",
     "debug_one_c_step",
+    "BandTimingStats",
+    "FrequencyBand",
+    "FrequencyContinuationResult",
+    "run_frequency_continuation",
 ]
 
