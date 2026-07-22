b902848312320f7bd4266ea0244e2400d3d64ae8
ada-ab2125 <ab2125@ic.ac.uk>
2026-07-22 17:18:58 +0100

    feat: api exposed

--- odil_wave/__init__.py
+++ odil_wave/__init__.py
@@ -11,7 +11,17 @@ from odil_wave.loss import (
     InverseLoss,
     Regulariser,
 )
-from odil_wave.optimisation import LBFGSB, debug_one_c_step
+from odil_wave.optimisation import (
+    LBFGSB,
+    LBFGSClosedForm,
+    MODILInversion,
+    MODILVelocityParameterization,
+    build_grid_hierarchy,
+    debug_one_c_step,
+    prolongate_interior_velocity,
+    resample_full_wavefield,
+    resample_interior_field,
+)
 
 __all__ = [
     "Grid",
@@ -29,5 +39,14 @@ __all__ = [
     "InverseLoss",
     "Regulariser",
     "LBFGSB",
+    "LBFGSClosedForm",
     "debug_one_c_step",
+    # genuine simultaneous mODIL
+    "MODILVelocityParameterization",
+    "MODILInversion",
+    "build_grid_hierarchy",
+    # geometry-aware grid transfer
+    "prolongate_interior_velocity",
+    "resample_interior_field",
+    "resample_full_wavefield",
 ]
--- odil_wave/optimisation/__init__.py
+++ odil_wave/optimisation/__init__.py
@@ -1,8 +1,31 @@
 from .base import LBFGSB
+from .closed_form import LBFGSClosedForm
 from .c_step_debug import debug_one_c_step
+from .grid_transfer import (
+    SUPPORTED_STAGGERINGS,
+    assert_compatible_interiors,
+    prolongate_interior_velocity,
+    resample_full_wavefield,
+    resample_interior_field,
+)
+from .modil import (
+    MODILInversion,
+    MODILVelocityParameterization,
+    build_grid_hierarchy,
+)
 
 __all__ = [
     "LBFGSB",
+    "LBFGSClosedForm",
     "debug_one_c_step",
+    # geometry-aware grid-transfer operators
+    "prolongate_interior_velocity",
+    "resample_interior_field",
+    "resample_full_wavefield",
+    "assert_compatible_interiors",
+    "SUPPORTED_STAGGERINGS",
+    # genuine simultaneous mODIL
+    "MODILVelocityParameterization",
+    "MODILInversion",
+    "build_grid_hierarchy",
 ]
-
