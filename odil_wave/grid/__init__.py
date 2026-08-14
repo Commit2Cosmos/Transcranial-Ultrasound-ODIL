4045bbd65b5c2a87b7ffd19f1ee82e7be90d6057
ada-mp425 <milica.pejic25@ic.ac.uk>
2026-08-14 23:27:53 +0100

    remove frequency limit validation and related api

--- odil_wave/grid/__init__.py
+++ odil_wave/grid/__init__.py
@@ -1,21 +1,15 @@
 from .grid import Grid
 from .frequency_selection import (
-    FrequencyLimits,
     FrequencySelection,
     complex_dtype,
-    default_min_ppw,
     discrete_lambda_t,
     discrete_lambda_tt,
-    usable_frequency_limits,
 )
 
 __all__ = [
     "Grid",
-    "FrequencyLimits",
     "FrequencySelection",
     "complex_dtype",
-    "default_min_ppw",
     "discrete_lambda_t",
     "discrete_lambda_tt",
-    "usable_frequency_limits",
 ]
